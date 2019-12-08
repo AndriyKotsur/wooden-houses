0012c3e55e71d9dab7309cffd51c9cbe3aa73bdb
myroslav.tokar96 <myroslav.tokar96@gmail.com>
2019-12-08 14:30:11 +0300

    Slideshow

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+"use strict";
+let slideIndex = 1;
+
+function showSlides(n) {
+    let slides = document.querySelectorAll(".slides-item");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    };
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    };
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+};
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+};
