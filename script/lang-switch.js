c3131ac725d15445d5c69cf68aaf4d87afd10326
Andriy Kotsur <kotsurandriy@gmail.com>
2019-12-30 16:22:39 +0200

    Corrected path content

--- script/lang-switch.js
+++ script/lang-switch.js
@@ -1,2 +1,15 @@
 "use strict";
-
+let lang = document.getElementById('lang');
+lang.onclick = function () {
+    let link = document.getElementById('lang_switch')
+    if (!link) {
+        return false
+    };
+    if (link.title == 'ua') {
+        link.href = '../rus/index.html';
+    } else if (link.title == 'rus') {
+        link.href = '../eng/index.html';
+    } else {
+        link.href = '../ua/index.html';
+    }
+};
