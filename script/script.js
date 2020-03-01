27f29f5a4c6c31895545c56821cfbb8534fd90bd
AndriyKotsur <kotsurandriy@gmail.com>
2020-03-01 17:56:47 +0200

    minor bugs fix

--- script/script.js
+++ script/script.js
@@ -13,65 +13,68 @@ window.addEventListener('load', function () {
             button.classList.toggle('active');
             buttonMenu.classList.toggle('active');
         });
-    })
+    });
 
-/* SLIDER */
-let slideIndex = 1;
+    /* SLIDER */
+    let slideIndex = 1;
 
-function showSlides(n) {
-    let slides = document.querySelectorAll(".sliderItems");
-    let dots = document.querySelectorAll(".dot-items");
-    if (slides.length == 0) {
-        return false;
-    } else if (n > slides.length) {
-        slideIndex = 1;
-    } else if (n < 1) {
-        slideIndex = slides.length;
+    function showSlides(n) {
+        let slides = document.querySelectorAll(".sliderItems");
+        let dots = document.querySelectorAll(".dot-items");
+        if (slides.length == 0) {
+            return false;
+        } else if (n > slides.length) {
+            slideIndex = 1;
+        } else if (n < 1) {
+            slideIndex = slides.length;
+        };
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" dot-items-active", "")
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " dot-items-active";
     };
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+
+    showSlides(slideIndex);
+    /* PREV BTN */
+    let prevBtn = document.querySelector('.prevBtn');
+    if (prevBtn) {
+        prevBtn.addEventListener('click', function () {
+            showSlides(slideIndex -= 1);
+        });
+    };
+    /* NEXT BTN */
+    let nextBtn = document.querySelector('.nextBtn');
+    if (nextBtn) {
+        nextBtn.addEventListener('click', function () {
+            showSlides(slideIndex += 1);
+        });
+    };
+    /* SLIDESHOW */
+    let sliderGallery = document.querySelector('.sliderGallery');
+    if (sliderGallery) {
+        sliderGallery.addEventListener('click', function () {
+            let slider = event.target.closest('img');
+            let arrSlides = [];
+            arrSlides[slider.id - 1] = slider;
+            let idx = arrSlides.indexOf(slider);
+            showSlides(slideIndex = ++idx);
+        });
+    };
+
+    /* DOTS CONTROL */
+    let sliderDots = document.querySelector('.sliderDots');
+    if (sliderDots) {
+        sliderDots.addEventListener('click', function () {
+            let dot = event.target.closest('span');
+            let arrDots = [];
+            arrDots[dot.id - 1] = dot;
+            let idx = arrDots.indexOf(dot);
+            showSlides(slideIndex = ++idx);
+        });
     }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dot-items-active", "")
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " dot-items-active";
-};
-
-showSlides(slideIndex);
-/* PREV BTN */
-let prevBtn = document.querySelector('.prevBtn');
-if (prevBtn) {
-    prevBtn.addEventListener('click', function () {
-        showSlides(slideIndex -= 1);
-    });
-}
-/* NEXT BTN */
-let nextBtn = document.querySelector('.nextBtn');
-if (nextBtn) {
-    nextBtn.addEventListener('click', function () {
-        showSlides(slideIndex += 1);
-    });
-}
-/* SLIDESHOW */
-let sliderGallery = document.querySelector('.sliderGallery');
-if (sliderGallery) {
-    sliderGallery.addEventListener('click', function () {
-        let slider = event.target.closest('img');
-        let arrSlides = [];
-        arrSlides[slider.id - 1] = slider;
-        let idx = arrSlides.indexOf(slider);
-        showSlides(slideIndex = ++idx);
-    });
-}
-
-/* DOTS CONTROL */
-let sliderDots = document.querySelector('.sliderDots'); sliderDots.addEventListener('click', function () {
-    let dot = event.target.closest('span');
-    let arrDots = [];
-    arrDots[dot.id - 1] = dot;
-    let idx = arrDots.indexOf(dot);
-    showSlides(slideIndex = ++idx);
-})
-})
+});
